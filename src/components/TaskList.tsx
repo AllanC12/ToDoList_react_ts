7982d36f4be9d4500738c6932e6bbaf924c00762
AllanC12 <allandevfront@gmail.com>
2023-05-11 22:17:08 -0300

    css de tasklist finalizado

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -15,9 +15,14 @@ const TaskList = ({taskList}: Props) => {
      <>
         {taskList.length > 0 ?(
           taskList.map(task=> (
-            <div key={task.id}>
-              <div>
-                 {task.title}
+            <div className={styles.task} key={task.id}>
+              <div className={styles.details}>
+                <h4>{task.title}</h4>
+                <p>{task.difficulty}</p>
+              </div>
+              <div className={styles.actions}>
+                <i className="bi bi-pencil"></i>
+                <i className="bi bi-trash"></i>
               </div>
             </div>  
           ))
