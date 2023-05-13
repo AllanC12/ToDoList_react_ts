25081ca1bd2cbf2a4de75cc24dc3081768c964a4
AllanC12 <allandevfront@gmail.com>
2023-05-12 22:06:07 -0300

    implementando funcionalide de fechar modal clicando fora dele

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -6,10 +6,15 @@ interface Props {
    children: React.ReactNode
 }
 
+const closeModal = (e:React.MouseEvent): void => {
+  const modal = document.querySelector('#modal')
+  modal!.classList.add('hide')
+}
+
 const Modal = ({children}: Props) => {
   return (
-    <div id='modal' >
-        <div className={styles.fade}></div>
+    <div id='modal'>
+        <div className={styles.fade} onClick={closeModal}></div>
 
         <div className={styles.modal}>
             <h2>Sua tarefa</h2>
